4a96a625bc78b7cd93bf18c36dcb02a8bed9ec3d
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-04-20 18:06:32 -0600

    endpoints - usuario

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -8,9 +8,35 @@ const pool = new Pool({
     port: '5432'
 });
 
-const get = async (req, res) => {
-    const response = await pool.query("SELECT * FROM usuarios");
+const getUsuarios = async (req, res) => {
+    const response = await pool.query('SELECT * FROM usuarios');
+    res.json({rows: response.rows});
+};
+
+const getUsuario = async(req, res) => {
+    const response = await pool.query("SELECT * FROM usuarios WHERE id_usuario=$1", [req.params.id]);
+    res.json(response.rows[0]);
+}
+ 
+const getUsuarioByNombre = async(req, res) => {
+    const response = await pool.query("SELECT * FROM usuarios WHERE LOWER(nombre) LIKE LOWER('%"+req.params.nombre+"%')");
     res.json(response.rows[0]);
 }
 
-module.exports = { get}
+const addUsuario = async (req, res) => {
+    const {nombre, correo, contrasena, telefono, historial} = req.query;
+    const response = await pool.query("INSERT INTO usuarios VALUES (default, $1, $2, crypt($3, gen_salt('bf')), $4, $5)", [nombre, correo, contrasena, telefono, historial]);
+    res.json({
+        message: 'User added',
+        body: {
+            user: {nombre, correo, contrasena, telefono, historial}
+        }
+    })
+};
+
+module.exports = { 
+    getUsuarios,
+    getUsuario,
+    getUsuarioByNombre,
+    addUsuario
+}
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 
-const { get } = require('../controllers/index.controller');
+const { getUsuarios, getUsuario, getUsuarioByNombre, addUsuario } = require('../controllers/index.controller');
 
-router.get('', get);
+router.get('/usuarios', getUsuarios);
+router.get('/usuario/:id', getUsuario);
+router.get('/usuarios/:nombre', getUsuarioByNombre);
+
+router.post('/usuario/', addUsuario);
 
 module.exports = router;
