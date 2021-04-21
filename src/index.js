70fc55a31d7a3e39cdcb692c1fee242534dc5f11
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-04-20 18:09:10 -0600

    moved endpoint

--- src/index.js
+++ src/index.js
@@ -10,18 +10,3 @@ app.use(require('./routes/index.routes'));
 
 app.listen(3000);
 console.log('Server on port 3000');
-
-app.post("/register", (req, res) => {
-
-    const username = req.body.username;
-    const password = req.body.password;
-  
-       pool.query(
-        "INSERT INTO usuario (id_usuario, contraseña) VALUES ($1, $2)",
-        [username, password],
-        (err, result) => {
-          console.log(err);
-          res.status(200).send("Usuario registrado");
-        }
-      );
-    });
