3596f58d6220d6cc7862cee56605721ea22444bc
wsaldana <53311124+wsaldana@users.noreply.github.com>
2021-08-08 14:31:16 -0600

    Added primary key to endpoint post method

--- server/src/controllers/index.controller.js
+++ server/src/controllers/index.controller.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-//const parameters = require('./parameters');
 const { NODE_ENV } = require('../../config');
 
 const pool = new Pool({
@@ -80,8 +79,9 @@ const getPacientesByMedico = async (req, res) => {
 }
 
 const addRegistroSintomas = async (req, res) => {
+    console.log(req.body)
     const {dolor_cabeza, molestia_espalda_baja, diarrea, sangrados, calambres, presion, niveles_azucar, peso, fecha, id_usuario} = req.body;
-    const response = await pool.query("INSERT INTO sintomas VALUES (default, $1, $2, $3, $4, $5, $6, $7, $8, $9, $10)", [dolor_cabeza, molestia_espalda_baja, diarrea, sangrados, calambres, presion, niveles_azucar, peso, fecha, id_usuario]);
+    const response = await pool.query("INSERT INTO sintomas VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, default)", [dolor_cabeza, molestia_espalda_baja, diarrea, sangrados, calambres, presion, niveles_azucar, peso, fecha, id_usuario]);
     res.json({
         statusCode: 200,
         message: 'Symptoms added',
