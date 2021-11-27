7b69071f2b4cd6bd12b2933fe87fe2896603709a
Daniela Villamar Rosales <86308071+danielavillamarrosales@users.noreply.github.com>
2021-11-27 13:33:19 -0600

    Flutter and end point
    push stashed changes

--- server/src/controllers/index.controller.js
+++ server/src/controllers/index.controller.js
@@ -9,20 +9,7 @@ const pool = new Pool({
     'port': process.env.DB_PORT
 });
 
-const login = async(req, res) => {
-    const { email, password } = req.headers;
-    const response = await pool.query("SELECT * FROM usuarios WHERE correo=$1 AND contrasena=crypt($2, contrasena)", [email, password]);
-    if(response.rowCount === 1){
-        res.json({
-            statusCode: 200,
-            body: response.rows[0]
-        });
-    }else{
-        res.json({
-            statusCode: 404
-        });
-    }
-};
+
 
 const getUsuarios = async (req, res) => {
     const response = await pool.query('SELECT * FROM usuarios');
@@ -39,17 +26,7 @@ const getUsuarioByNombre = async(req, res) => {
     res.json(response.rows[0]);
 }
 
-const addUsuario = async (req, res) => {
-    const {nombre, correo, contrasena, telefono, historial} = req.body;
-    const response = await pool.query("INSERT INTO usuarios VALUES (default, $1, $2, crypt($3, gen_salt('bf')), $4, $5)", [nombre, correo, contrasena, telefono, historial]);
-    res.json({
-        statusCode: 200,
-        message: 'User added',
-        body: {
-            user: {nombre, correo, contrasena, telefono, historial}
-        }
-    })
-};
+
 
 const getHistoriales = async (req, res) => {
     const response = await pool.query('SELECT * FROM historial_medico');
